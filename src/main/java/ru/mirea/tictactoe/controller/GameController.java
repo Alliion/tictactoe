198eb77c3557e3ddc9c86e4173b3944c996dde2a
Vadim Abramov <vadimabramov322@gmail.com>
2021-12-07 01:43:41 +0300

    небольшой фикс для выполнения запросов с фронта без проблем

--- src/main/java/ru/mirea/tictactoe/controller/GameController.java
+++ src/main/java/ru/mirea/tictactoe/controller/GameController.java
@@ -10,21 +10,25 @@ import ru.mirea.tictactoe.service.GameService;
 public class GameController {
     private final GameService service;
 
+    @CrossOrigin
     @GetMapping("/game")
     public Game getUserGame(@RequestParam(name = "user_id") String userId) {
         return service.getUserGame(userId);
     }
 
+    @CrossOrigin
     @PostMapping("/game")
     public Game createUserGame(@RequestParam(name = "user_id") String userId) {
         return service.createGame(userId);
     }
 
+    @CrossOrigin
     @PostMapping("/game/join")
     public Game joinGame(@RequestParam(name = "game_id") long gameId, @RequestParam(name = "user_id") String userId) {
         return service.joinGame(gameId, userId);
     }
 
+    @CrossOrigin
     @PostMapping("/game/move")
     public Game makeMove(@RequestParam(name = "user_id") String userId, @RequestParam(name = "x")  int x, @RequestParam(name = "y") int y) {
         return service.makeMove(userId, x, y);
